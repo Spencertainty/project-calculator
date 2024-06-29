eca0123beae966326c2a9976aa14aef25b16416f
Spencertainty <sjmfeagin@gmail.com>
2024-06-29 10:43:37 -0600

    added updateDisplay function and eventListener

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+
+function operate(operator, a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;            
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -17,6 +38,41 @@ function divide(a, b) {
         return a / b;
     }
 }
+
+function updateDisplay(value) {
+    const display = document.getElementById('display');
+    display.textContent = value;
+}
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.textContent;
+        if (button.id === 'clear') {
+            firstNumber = '';
+            operator = '';
+            secondNumber - '';
+            updateDisplay('0');
+        } else if (['+', '-', '*', '/'].includes(value)) {
+            operator = value;
+        } else if (value === '=') {
+            if (firstNumber && operator && secondNumber) {
+                const result = operate(operator, firstNumber, secondNumber);
+                updateDisplay(result);
+                firstNumber = result;
+                operator = '';
+                secondNumber = '';
+            }
+        } else {
+            if (operator) {
+                secondNumber += value;
+                updateDisplay(secondNumber);
+            } else {
+                firstNumber += value;
+                updateDisplay(firstNumber);
+            }
+        }
+    });
+});
 
 console.log(add(2, 3));
 console.log(subtract(5, 2));
